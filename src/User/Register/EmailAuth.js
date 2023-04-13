299520e774fc4b32f22fbd6fada7c8e5149a834f
201724554 <pay7845@naver.com>
2023-04-14 01:15:53 +0900

    #18-style/<h2> 추가

--- src/User/Register/EmailAuth.js
+++ src/User/Register/EmailAuth.js
@@ -1,5 +1,5 @@
 import { customAxios } from "../../Common/CustomAxios";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import LoadingSpinner from "../../Common/LoadingSpinner";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -11,40 +11,41 @@ function EmailAuth() {
     const [email, setEmail] = useState("");
     const [authNum, setAuthNum] = useState("");
 
-    useEffect(() => {
-        if (state === null || state === undefined) {
+    useEffect(()=>{
+        if(state === null || state === undefined) {
             alert("잘못된 접근입니다");
             navigate("/");
         }
-    }, [])
+    },[])
 
     function sendAuthNum() {
         setSpinner(true);
-        customAxios.post("/auth", { email: email }).then(() => {
+        customAxios.post("/auth", {email: email}).then(() => { 
             setSpinner(false);
-            alert("인증번호를 메일로 전송했습니다");
-        }).finally(() => setSpinner(false));
+            alert("인증번호를 메일로 전송했습니다"); 
+        }).finally(()=>setSpinner(false));
     }
 
     function confirmAuthNum() {
         setSpinner(true);
-        customAxios.get("/auth", { params: { email: email, authNum: authNum } }).then(() => {
+        customAxios.get("/auth", {params: {email: email, authNum: authNum}}).then(()=>{
             alert("인증이 완료되었습니다");
-            navigate("/register", { state: { role: state.role, email: email } });
-        }).finally(() => setSpinner(false));
+            navigate("/register", {state: {role: state.role, email: email}}); 
+        }).finally(()=>setSpinner(false));
     }
 
     return (
         <div>
+            <h2>이메일 인증</h2>
             {
-                spinner === true ? (<LoadingSpinner />) : (<></>)
+                spinner === true ? (<LoadingSpinner/>) : (<></>)
             }
-            <div style={spinner ? { pointerEvents: "none", opacity: "0.4" } : {}}>
+            <div style={spinner ? {pointerEvents: "none", opacity: "0.4"} : {}}>
                 <div>
-                    <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="이메일" /><button type="button" onClick={sendAuthNum}>인증 번호 (재)전송</button>
+                    <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder="이메일" /><button type="button" onClick={sendAuthNum}>인증 번호 (재)전송</button>
                 </div>
                 <div>
-                    <input type="text" onChange={(e) => setAuthNum(e.target.value)} placeholder="인증번호" /><button type="button" onClick={confirmAuthNum}>인증하기</button>
+                    <input type="text" onChange={(e)=>setAuthNum(e.target.value)} placeholder="인증번호" /><button type="button" onClick={confirmAuthNum}>인증하기</button>
                 </div>
             </div>
         </div>
