030829e3cb7658a40a89a3fb2085a926406ea20f
201724554 <pay7845@naver.com>
2023-04-19 03:01:38 +0900

    #18-feat/admin 로그인 페이지 추가

---
+++ src/Admin/Login/AdminLogin.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { customAxios } from "../../Common/CustomAxios";
+import { useNavigate } from "react-router-dom";
+
+
+function AdminLogin() 
+{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    function submit() 
+    {
+        customAxios.post("/login/admin", {username: username, password: password})
+        .then(()=>navigate("/"));
+    }
+
+    return (
+    <div>
+        <input type="text" required={true} placeholder="아이디" onChange={(e)=>setUsername(e.target.value)}/>
+        <input type="password" required={true} placeholder="비밀번호" onChange={(e)=>setPassword(e.target.value)}/>
+        <button type="button" onClick={submit}/>
+    </div>
+    );
+}
+
+export default AdminLogin;
