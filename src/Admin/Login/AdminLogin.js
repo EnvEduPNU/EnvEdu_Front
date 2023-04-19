0abca801846c6cc919d3b1095d9b5a7d9f2c1d03
201724554 <pay7845@naver.com>
2023-04-20 03:23:29 +0900

    #18-feat/어드민 로그인 페이지 추가

--- src/Admin/Login/AdminLogin.js
+++ src/Admin/Login/AdminLogin.js
@@ -19,7 +19,7 @@ function AdminLogin()
     <div>
         <input type="text" required={true} placeholder="아이디" onChange={(e)=>setUsername(e.target.value)}/>
         <input type="password" required={true} placeholder="비밀번호" onChange={(e)=>setPassword(e.target.value)}/>
-        <button type="button" onClick={submit}/>
+        <button type="button" onClick={submit}>로그인</button>
     </div>
     );
 }
