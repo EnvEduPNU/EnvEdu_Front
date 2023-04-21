2a73fcb34c3cd502a02a8c3dbff24c7edd74e0da
201724554 <pay7845@naver.com>
2023-04-21 18:20:10 +0900

    #18-feat/index.js에 CookiesProvider 적용

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </BrowserRouter>
 );
 
