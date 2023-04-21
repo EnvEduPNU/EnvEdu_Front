80aa5a24c4140c4fb325ba7e9d2b03d85e08325a
201724554 <pay7845@naver.com>
2023-04-21 21:11:17 +0900

    #18-feat/global axios에 withCredential 옵션 추가

--- src/Common/CustomAxios.js
+++ src/Common/CustomAxios.js
@@ -3,7 +3,8 @@ import {RESPONSE_FORBIDDEN, RESPONSE_UNAUTHORIZED} from "./Response";
 import {isExpired} from "react-jwt";
 
 export const customAxios = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}`
+    baseURL: `${process.env.REACT_APP_API_URL}`,
+    withCredentials: true
 })
 
 customAxios.interceptors.request.use(
