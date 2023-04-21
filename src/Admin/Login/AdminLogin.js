ef23b129b807c0c96e9239642c30ba0f33944ce9
201724554 <pay7845@naver.com>
2023-04-22 03:06:29 +0900

    #18-feat/useCookie 제거

--- src/Admin/Login/AdminLogin.js
+++ src/Admin/Login/AdminLogin.js
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import { customAxios } from "../../Common/CustomAxios";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from 'react-cookie';
 
 function AdminLogin() 
 {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [adminCookie, setAdminCookie] = useCookies("access_token");
     const navigate = useNavigate();
 
     function submit() 
     {
         customAxios.post("/login/admin", {username: username, password: password})
-        .then((res)=>{
-            console.log(res)
-            //navigate("/")
+        .then(()=>{
+            navigate("/");
         });
     }
 
