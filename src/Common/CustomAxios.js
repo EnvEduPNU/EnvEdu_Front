cd2ea9291122b7777152022f9bbb7df5c14f51ec
201724554 <pay7845@naver.com>
2023-04-28 04:19:40 +0900

    #18-feat/로그인 성공 시 axios interceptor에서 유저 정보 저장

--- src/Common/CustomAxios.js
+++ src/Common/CustomAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import {RESPONSE_FORBIDDEN, RESPONSE_UNAUTHORIZED} from "./Response";
-import {isExpired} from "react-jwt";
+import {RESPONSE_BAD_REQ, RESPONSE_FORBIDDEN, RESPONSE_UNAUTHORIZED} from "./Response";
+import {decodeToken} from "react-jwt";
 
 export const customAxios = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}`,
@@ -8,45 +8,37 @@ export const customAxios = axios.create({
 })
 
 customAxios.interceptors.request.use(
-    function (config)
-    {
+    function (config) {
         config.headers.ContentType = "application/json; charset=utf-8";
         config.headers.authorization = localStorage.getItem("jwt");
-        if(isExpired(localStorage.getItem("jwt")))
-        {
-            if(localStorage.getItem("refresh") != null)
-            {
-                config.headers.refresh = localStorage.getItem("refresh");
-            }
-        }
         return config;
     }
 )
 
 customAxios.interceptors.response.use(
-    function (response)
-    {
-        if(response.headers['authorization'] !== undefined)
-        {
-            localStorage.setItem("jwt", response.headers['authorization']);
-        }
-        if(response.headers['refresh'] !== undefined)
-        {
-            localStorage.setItem("refresh", response.headers['refresh']);
+    function (response) {
+        if(response.headers['authorization'] !== undefined) {
+            let accessToken = response.headers['authorization'];
+            localStorage.setItem("access_token", accessToken);
+            
+            let username = decodeToken(accessToken).user_info.username;
+            let role = decodeToken(accessToken).user_info.role;
+
+            localStorage.setItem("username", username);
+            localStorage.setItem("role", role);
         }
         return response;
     },
     function (error) {
-        if (error.response.request.status === RESPONSE_UNAUTHORIZED)
-        {
+        if (error.response.request.status === RESPONSE_UNAUTHORIZED) {
             alert("로그인 해주세요");
+            window.location.reload();
             localStorage.clear();
         }
-        else if(error.response.request.status === RESPONSE_FORBIDDEN)
-        {
+        else if(error.response.request.status === RESPONSE_FORBIDDEN) {
             alert("권한이 없습니다");
         }
-        else {
+        else if(error.response.request.status === RESPONSE_BAD_REQ) {
             alert(error.response.data);
         }
         return Promise.reject(error);
