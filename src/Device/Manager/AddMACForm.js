b95256650fb197e45c83d02b16fcc0eb3ca476ce
201724554 <pay7845@naver.com>
2023-05-05 02:40:33 +0900

    #19-기존의 form을 ExtendableInputs으로 변경

--- src/Device/Manager/AddMACForm.js
+++ src/Device/Manager/AddMACForm.js
@@ -1,28 +1,42 @@
-import {useForm} from "react-hook-form";
-import {customAxios} from "../../Common/CustomAxios";
+import { useState } from 'react';
+import { customAxios } from "../../Common/CustomAxios";
+import ExtendableInputs from "../../Common/ExtendableInputs";
 
 function AddMACForm() {
-    const {register,handleSubmit,formState: {errors}} = useForm();
+    const [inputs, setInputs] = useState([]);
 
-    function onSubmit(data) {
-        if(window.confirm("MAC 주소를 확인하셨나요?")) {
-            customAxios.post("/admin/device", {...data})
-                .then(() => {
-                    alert("등록 완료");
-                })
-        }
+    function submit() {
+        customAxios.post("/admin/device", { macs: inputs })
+            .then(() => {
+                alert("done");
+            });
     }
+    // const {register,handleSubmit,formState: {errors}} = useForm();
 
+    // function onSubmit(data) {
+    //     if(window.confirm("MAC 주소를 확인하셨나요?")) {
+    //         customAxios.post("/admin/device", {...data})
+    //             .then(() => {
+    //                 alert("등록 완료");
+    //             })
+    //     }
+    // }
+
+    // return (
+    //     <>
+    //         <form onSubmit={handleSubmit(onSubmit)}>
+    //             <div>
+    //                 <input placeholder="00:00:00:00:00:00" {...register("mac", {required: {value: true, message: "MAC 주소를 입력하세요"}})}/>
+    //                 {errors.MAC && <span style={{color:'red', fontSize:"13px"}}>{errors.MAC.message}</span>}
+    //             </div>
+    //             <button type="submit">추가하기</button>
+    //         </form>
+    //     </>
+    // );
     return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <input placeholder="00:00:00:00:00:00" {...register("mac", {required: {value: true, message: "MAC 주소를 입력하세요"}})}/>
-                    {errors.MAC && <span style={{color:'red', fontSize:"13px"}}>{errors.MAC.message}</span>}
-                </div>
-                <button type="submit">추가하기</button>
-            </form>
-        </>
+        <div>
+            <ExtendableInputs submit={submit} inputs={inputs} setInputs={setInputs} placeholder={"00:00:00:00:00:00"}/>
+        </div>
     );
 }
 
