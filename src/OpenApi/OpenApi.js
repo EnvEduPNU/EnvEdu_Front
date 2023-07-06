ef723d02a13e0647f3ca9e7d31b65d1c404c6985
Jisu Lee <dlwltn0430@naver.com>
2023-07-07 02:03:26 +0900

    #23-feat/그래프 표현 기능 1차 구현

--- src/OpenApi/OpenApi.js
+++ src/OpenApi/OpenApi.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './OpenApi.css';
 import {customAxios} from "../Common/CustomAxios";
 import earthImg from "./earth.png"
@@ -6,7 +6,10 @@ import earthImg from "./earth.png"
 //추후 삭제 필요
 import waterdata from "./data.json" ;
 import airdata from "./data2.json";
-import { useEffect } from 'react';
+
+//그래프 그리기
+import { Chart } from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
 
 //로딩 시간 줄이려면 useEffect로 미리 res.data 다 받아온 다음에 
 //'수질', '대기질' 중 선택하는 항목에 따라 다른 component 보여줘야 함
@@ -90,7 +93,7 @@ function OpenApi() {
         } else if (category === 'OCEAN') {
             path = '/ocean-quality';
         }
-        customAxios.post(path,selectedItems)
+        customAxios.post(path, selectedItems)
         .then( () => {
             alert("데이터 저장을 성공했습니다!");
         })
@@ -105,7 +108,7 @@ function OpenApi() {
     }
     */}
 
-    {/*필터링을 위해 addr 선택*/}
+    {/* 필터링을 위해 addr 선택 */}
     function handleSelectChange (e) {
         if (e.target.value === '전체') {
             setSelectedOption(null);
@@ -150,135 +153,201 @@ function OpenApi() {
         }
     }
 
+    /*그래프 그리기 */
+    console.log(selectedItems);
+    const graphLabel = selectedItems.map((selectedItem) => selectedItem.stationName);
+    const oceanData_ITEMCOD = selectedItems.map((selectedItem) => selectedItem.ITEMCOD);
+    const oceanData_ITEMDOC = selectedItems.map((selectedItem) => selectedItem.ITEMDOC);
+    //const airData = selectedItems.map((selectedItem) => selectedItem.pm10Value);
+
+    /* useEffect 사용하거나 버튼 살려서 완성하기 */
+    if (category === 'OCEAN') {
+
+    } else if (category === 'AIR') {
+
+    }
+    
+    let sample = {
+        labels: graphLabel,
+        datasets: [
+          {
+            type: 'bar',
+            label: 'ITEMCOD',
+            backgroundColor: '#8bc34a',
+            data: oceanData_ITEMCOD
+          },
+          {
+            type: 'bar',
+            label: 'ITEMDOC',
+            backgroundColor: '#558b2f',
+            data: oceanData_ITEMDOC,
+          },
+        ],
+      };
+
+    /*
+    const drawGraph = () => {
+        if (graphLabel.length === 0) {
+            alert("데이터를 선택해주세요");
+        } else {
+            
+        }
+    }
+    */
+
+    console.log(category)
+
     return (
         <div style={{margin: 'auto'}}>
-        <div id="wrap-openapi-div">
-            {/*
-            <div className={showModal ? 'modal-full' : ''} onClick={modalClick} style={{background: 'pink'}}></div>
-            {showModal &&
-                <div className="modal-table">
-                    <div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    {headers.map((header) => (
-                                        <th key={header} onClick={modalClick}>{header}</th>
+            <div id="wrap-openapi-div">
+                {/*
+                <div className={showModal ? 'modal-full' : ''} onClick={modalClick} style={{background: 'pink'}}></div>
+                {showModal &&
+                    <div className="modal-table">
+                        <div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        {headers.map((header) => (
+                                            <th key={header} onClick={modalClick}>{header}</th>
+                                        ))}
+                                        <th>확인</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {Object.values(selectedItem).map((value, index) => (
+                                        <td key={index} onClick={modalClick}>{value}</td>
                                     ))}
-                                    <th>확인</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {Object.values(selectedItem).map((value, index) => (
-                                    <td key={index} onClick={modalClick}>{value}</td>
-                                ))}
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                }
+                */}
+                <h3 className="air-div-full">
+                    <img src={earthImg} 
+                        style={{
+                            width: '3.125rem', 
+                            marginRight: '0.625rem'
+                            }}/>
+                    부산 수질/대기질 데이터 조회
+                </h3>
+
+                <div className="wrap-select-type">
+                    <div onClick={handleButtonClickOcean} className="select-type">
+                        <span>수질 데이터 조회</span>
+                    </div>
+                    <div onClick={handleButtonClickAir} className="select-type">
+                        <span>대기질 데이터 조회</span>
                     </div>
                 </div>
-            }
-            */}
-            <h3 className="air-div-full">
-                <img src={earthImg} 
-                    style={{
-                        width: '3.125rem', 
-                        marginRight: '0.625rem'
-                        }}/>
-                부산 환경 상태
-            </h3>
-            <div className="wrap-select-type">
-                <div onClick={handleButtonClickOcean} className="select-type">
-                    <span>수질 데이터 조회</span>
-                </div>
-                <div onClick={handleButtonClickAir} className="select-type">
-                    <span>대기질 데이터 조회</span>
-                </div>
-            </div>
             
-            
-            {/*여기서부터 */}
-            <div style={{marginTop: '1.875rem'}}> 
-                <label className="filter-label" style={{marginRight: '0.625rem'}}>조사 지점 필터링</label>
-                <select
-                    value={selectedOption ? selectedOption.stationName : ''}
-                    onChange={handleSelectChange}
-                    className="air-buttons"
-                >   
-                    <option key="전체" value="전체">전체</option>
-                    {options.map((option) => (
-                        <option key={option.id} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            <div id="div-headers" style={{marginTop: '1rem'}}>
-                <label className="filter-label">추가/삭제</label>
-                <div 
-                    style={{
-                        margin: '0.625rem 0',
-                        display: 'flex', 
-                        justifyContent: 'space-between', 
-                        width: '100%'
-                    }}>
-                        {checkedHeaders.map((header) => (
-                            <label key={header}>
-                                <input
-                                    type="checkbox"
-                                    key={header}
-                                    name={header}
-                                    value={header}
-                                    checked={headers.includes(header)}
-                                    onChange={handlePropertyCheckboxChange}
-                                />
-                                {header}
-                            </label>
+                <div style={{marginTop: '1.875rem'}}> 
+                    <label className="filter-label" style={{marginRight: '0.625rem'}}>조사 지점 필터링</label>
+                    <select
+                        value={selectedOption ? selectedOption.stationName : ''}
+                        onChange={handleSelectChange}
+                        className="air-buttons"
+                    >   
+                        <option key="전체" value="전체">전체</option>
+                        {options.map((option) => (
+                            <option key={option.id} value={option.value}>
+                                {option.label}
+                            </option>
                         ))}
+                    </select>
                 </div>
-            </div>
-            
-            <div style={{
-                display: 'flex',
-                justifyContent: 'flex-end', 
-            }}>
-                <button id="save-my-data" onClick={handleSaveMyData}>
-                    데이터 저장하기
-                </button>
-            </div>
 
-            <table border="1" className="openAPI-table">
-                <thead>
-                    {headers.map((header) => (
-                        <th key={header}>{header}</th>
-                    ))}
-                    {isShow &&
-                        <th>
-                            <input
-                                type="checkbox"
-                                onChange={() => handleFullCheck()}
-                                checked={isFull}
-                            ></input>
-                        </th>
-                    }
-                </thead>
-                <tbody>
-                    {filteredData.map((item) => (
-                        <tr key={item.id}>
-                            {headers.map((header) => (
-                                <td key={header}>{item[header]}</td>
+                <div style={{marginTop: '1rem'}}>
+                    <label className="filter-label">추가/삭제</label>
+                    <div 
+                        style={{
+                            margin: '0.625rem 0',
+                            display: 'flex', 
+                            justifyContent: 'space-between', 
+                            width: '100%'
+                        }}>
+                            {checkedHeaders.map((header) => (
+                                <label key={header}>
+                                    <input
+                                        type="checkbox"
+                                        key={header}
+                                        name={header}
+                                        value={header}
+                                        checked={headers.includes(header)}
+                                        onChange={handlePropertyCheckboxChange}
+                                    />
+                                    {header}
+                                </label>
                             ))}
-                            <td>
+                    </div>
+                </div>
+                
+                <div style={{
+                    display: 'flex', 
+                    justifyContent: 'flex-end'
+                }}>
+                    <button 
+                        id="table-btn" 
+                        onClick={handleSaveMyData}
+                        style={{marginRight: '0.3rem'}} >
+                        데이터 저장하기
+                    </button>
+                    {/*
+                    <button 
+                        id="table-btn"
+                        onClick={drawGraph}>
+                        그래프 그리기
+                    </button>
+                    */}
+                </div>
+
+                <table border="1" className="openAPI-table">
+                    <thead>
+                        {headers.map((header) => (
+                            <th key={header}>{header}</th>
+                        ))}
+                        {isShow &&
+                            <th>
                                 <input
                                     type="checkbox"
-                                    name={item}
-                                    checked={selectedItems.includes(item)}
-                                    onChange={() => handleViewCheckBoxChange(item)}
+                                    onChange={() => handleFullCheck()}
+                                    checked={isFull}
                                 ></input>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                            </th>
+                        }
+                    </thead>
+                    <tbody>
+                        {filteredData.map((item) => (
+                            <tr key={item.id}>
+                                {headers.map((header) => (
+                                    <td key={header}>{item[header]}</td>
+                                ))}
+                                <td>
+                                    <input
+                                        type="checkbox"
+                                        name={item}
+                                        checked={selectedItems.includes(item)}
+                                        onChange={() => handleViewCheckBoxChange(item)}
+                                    ></input>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <div style={{
+                    marginTop: '1.875rem',
+                    display: 'flex',
+                    justifyContent: 'center'
+                    }}>                  
+                    <div style={{
+                        width: '50%'
+                        }}>
+                        <Line type="line" data={sample} />
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
