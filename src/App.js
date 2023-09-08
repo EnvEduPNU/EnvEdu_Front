eb11a53d709d85c2e6ada1f812572ae96d17cbc2
LeeJaeHoon <dlwogns3413@naver.com>
2023-09-08 10:27:19 +0900

    DataLiteracy-feat/그래프 그리기 변인 선택 구현

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import RegisterForm from './User/Register/RegisterForm';
-import HomePage from './HomePage';
-import AddMACForm from './Device/Manager/AddMACForm';
-import LoginForm from './User/Login/LoginForm';
-import ConnectPage from './Socket/ConnectPage';
-import Header from './Header/Header';
-import TestSocket from './Test/SocketTest';
-import TestFetch from './Test/TestFetch';
-import RegisterStudents from './User/Educator/RegisterStudents';
-import Footer from './Footer/Footer';
-import Find from './User/Find/Find';
-import Notice from './Board/Notice/Notice';
-import Board from './Board/Board';
-import Board_content from './Board/Board_content';
-import Team from './About/Team/Team';
-import What from './About/What/What';
-import Training from './Learnmore/Train/Training';
-import News from './Learnmore/News/News';
-import EmailAuth from './User/Register/EmailAuth';
-import AdminLogin from './Admin/Login/AdminLogin';
-import DeviceList from './Admin/Device/DeviceList';
-import ContactUs from './Contact/ContactUs';
-import Resource from './Learnmore/Resource/Resource';
-import Tboard from './Learnmore/Train/Tborad';
-import OpenApi from './OpenApi/OpenApi';
-import OpenApiPast from './OpenApi/openAPIPast';
-import Search from './OpenApi/search';
-import MyData from './myData/myData';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import RegisterForm from "./User/Register/RegisterForm";
+import HomePage from "./HomePage";
+import AddMACForm from "./Device/Manager/AddMACForm";
+import LoginForm from "./User/Login/LoginForm";
+import ConnectPage from "./Socket/ConnectPage";
+import Header from "./Header/Header";
+import TestSocket from "./Test/SocketTest";
+import TestFetch from "./Test/TestFetch";
+import RegisterStudents from "./User/Educator/RegisterStudents";
+import Footer from "./Footer/Footer";
+import Find from "./User/Find/Find";
+import Notice from "./Board/Notice/Notice";
+import Board from "./Board/Board";
+import Board_content from "./Board/Board_content";
+import Team from "./About/Team/Team";
+import What from "./About/What/What";
+import Training from "./Learnmore/Train/Training";
+import News from "./Learnmore/News/News";
+import EmailAuth from "./User/Register/EmailAuth";
+import AdminLogin from "./Admin/Login/AdminLogin";
+import DeviceList from "./Admin/Device/DeviceList";
+import ContactUs from "./Contact/ContactUs";
+import Resource from "./Learnmore/Resource/Resource";
+import Tboard from "./Learnmore/Train/Tborad";
+import OpenApi from "./OpenApi/OpenApi";
+import OpenApiPast from "./OpenApi/openAPIPast";
+import Search from "./OpenApi/search";
+import MyData from "./myData/myData";
 
-import Measure from './Education/Measure/measure';
-import Sample from './Socket/sample';
+import Measure from "./Education/Measure/measure";
+import Sample from "./Socket/sample";
 
-import MeasureContinuous from './Education/Measure/measure_continuous';
-
+import MeasureContinuous from "./Education/Measure/measure_continuous";
+import VariableSelection from "./DataLiteracy/DrawGraph/VariableSelection";
+import GraphSelection from "./DataLiteracy/DrawGraph/GraphSelection";
 
 function App() {
   /**
    * 처음에 반드시 .env 파일 생성 후 REACT_APP_API_URL = ${서버 도메인} 작성
    */
-  useEffect(()=>{
+  useEffect(() => {
     //todo: check validity of refresh token(cookie)
-  },[]);
+  }, []);
   return (
     <>
       <Header />
@@ -50,11 +51,11 @@ function App() {
         <Routes>
           {/*home*/}
           <Route index element={<HomePage />} />
-          
+
           {/*user*/}
-          <Route path="/auth" exact={true} element={<EmailAuth/>}/>
-          <Route path="/register" exact={true} element={<RegisterForm />}/>
-  
+          <Route path="/auth" exact={true} element={<EmailAuth />} />
+          <Route path="/register" exact={true} element={<RegisterForm />} />
+
           <Route
             path="/educator/student/add"
             exact={true}
@@ -69,50 +70,63 @@ function App() {
 
           {/*login*/}
           <Route path="/login" exact={true} element={<LoginForm />} />
-          
+
           {/*socket*/}
           <Route path="/socket" exact={true} element={<ConnectPage />} />
-          <Route path="/sample" exact={true} element={<Sample />}/>
-          
+          <Route path="/sample" exact={true} element={<Sample />} />
+
           {/*test*/}
           <Route path="/test/socket" exact={true} element={<TestSocket />} />
           <Route path="/test/fetch" exact={true} element={<TestFetch />} />
-          
+
           {/*id_password_find*/}
-          <Route path="/find" exact={true} element={<Find/>} />
-          
+          <Route path="/find" exact={true} element={<Find />} />
+
           {/*board*/}
-          <Route path="/board" exact={true} element={<Board/>} />
-          <Route path="/notice" exact={true} element={<Notice/>} />
-          <Route path="/boardcontent" exact={true} element={<Board_content/>} />
-          
+          <Route path="/board" exact={true} element={<Board />} />
+          <Route path="/notice" exact={true} element={<Notice />} />
+          <Route
+            path="/boardcontent"
+            exact={true}
+            element={<Board_content />}
+          />
+
           {/*About*/}
-          <Route path="/team" exact={true} element={<Team/>} />
-          <Route path="/what" exact={true} element={<What/>} />
+          <Route path="/team" exact={true} element={<Team />} />
+          <Route path="/what" exact={true} element={<What />} />
 
           {/*Education */}
           {/*<Route path="/E-Classes" exact={true} element={<Test/>} />*/}
-          <Route path="/measure" exact={true} element={<Measure/>} />
-          <Route path="/measureContinuous" exact={true} element={<MeasureContinuous/>} />
+          <Route path="/measure" exact={true} element={<Measure />} />
+          <Route
+            path="/measureContinuous"
+            exact={true}
+            element={<MeasureContinuous />}
+          />
 
           {/*Learnmore*/}
-          <Route path="/training" exact={true} element={<Training/>} />
-          <Route path="/news_reserch" exact={true} element={<News/>} />
-          <Route path="/resource" exact={true} element={<Resource/>} />
-          <Route path="/tboard" exact={true} element={<Tboard/>} />
+          <Route path="/training" exact={true} element={<Training />} />
+          <Route path="/news_reserch" exact={true} element={<News />} />
+          <Route path="/resource" exact={true} element={<Resource />} />
+          <Route path="/tboard" exact={true} element={<Tboard />} />
 
           {/*Admin*/}
-          <Route path="/admin/login" exact={true} element={<AdminLogin/>}/>
-          <Route path="/admin/devices" exact={true} element={<DeviceList/>}/>
+          <Route path="/admin/login" exact={true} element={<AdminLogin />} />
+          <Route path="/admin/devices" exact={true} element={<DeviceList />} />
           <Route
             path="/admin/add/device"
             exact={true}
             element={<AddMACForm />}
           />
 
-        {/*ContactUs*/}
-        <Route path="/contactus" exact={true} element={<ContactUs/>} />
-       
+          {/*ContactUs*/}
+          <Route path="/contactus" exact={true} element={<ContactUs />} />
+
+          {/*DataLiteracy */}
+          <Route path="/dataLiteracy/drawGraph">
+            <Route path="1" element={<VariableSelection />} />
+            <Route path="2" element={<GraphSelection />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
---
+++ src/DataLiteracy/DrawGraph/GraphSelection.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function GraphSelection() {
+  return (
+    <div>
+      <Button>추천 그래프 유형</Button>
+      <Button>그래프 선택하기</Button>
+    </div>
+  );
+}
+
+export default GraphSelection;
