7db2d6c2b3abf09c45b5b26a2c6407970202fb6d
LeeJaeHoon <dlwogns3413@naver.com>
2023-09-18 14:03:43 +0900

    #115 - refactor/LineAxisScaleStoe, barAxisScaleStor 공통 로직  Refactor

--- src/DataLiteracy/store/drawGraphStore.js
+++ src/DataLiteracy/store/drawGraphStore.js
@@ -19,57 +19,48 @@ export const useSelectedVariable = create(set => ({
   setSelectedVariable: variables => set({ selectedVariable: [...variables] }),
 }));
 
-export const useBarAxisSacleEditorStore = create(set => ({
-  axisScale: { x: [], y: [], min: 0, max: 0, stepSize: 0 },
-  // bubbleAxisScale: {
-  //   x: { selected: -1, min: 0, max: 0, stepSize: 0 },
-  //   y: { selected: -1, min: 0, max: 0, stepSize: 0 },
-  //   r: -1,
-  // },
-  // scatterAxisScale: {
-  //   x: { selected: -1, min: 0, max: 0, stepSize: 0 },
-  //   y: { selected: -1, min: 0, max: 0, stepSize: 0 },
-  // },
-  // mixChartAxisSale: {
-  //   x: -1,
-  //   y1: { selected: [], min: 0, max: 0, stepSize: 0 },
-  //   y2: { selected: [], min: 0, max: 0, stepSize: 0 },
-  //   selectedBar: [],
-  //   selectedLine: [],
-  // },
+// 공통 변경 함수
+const changeValue = (set, key) => value =>
+  set(state => ({
+    ...state,
+    axisScale: {
+      ...state.axisScale,
+      [key]: value,
+    },
+  }));
 
-  changeMinValue: min =>
+const changeSelected =
+  (set, axis, isLineChart) => (selectedIdx, qualitativeVariableIdx) =>
     set(state => {
-      return { ...state, axisScale: { ...state.axisScale, min } };
-    }),
+      const currentAxis = state.axisScale[axis];
 
-  changeMaxValue: max =>
-    set(state => {
-      return { ...state, axisScale: { ...state.axisScale, max } };
-    }),
+      if (isLineChart) {
+        if (axis === "x") {
+          if (selectedIdx !== qualitativeVariableIdx) {
+            alert("만들 수 없는 그래프 유형입니다.");
+            return state;
+          }
+        } else {
+          if (selectedIdx === qualitativeVariableIdx) {
+            alert("만들 수 없는 그래프 유형입니다.");
+            return state;
+          }
+        }
+      }
 
-  changeStepSize: stepSize =>
-    set(state => {
-      return { ...state, axisScale: { ...state.axisScale, stepSize } };
-    }),
-
-  changeSelectedX: (selectedIdx, qualitativeVariableIdx) =>
-    set(state => {
-      const { x } = state.axisScale;
-
-      if (x.includes(selectedIdx)) {
+      if (currentAxis.includes(selectedIdx)) {
         return {
           ...state,
           axisScale: {
             ...state.axisScale,
-            x: x.filter(s => s !== selectedIdx),
+            [axis]: currentAxis.filter(s => s !== selectedIdx),
           },
         };
       }
 
       if (
-        x.includes(qualitativeVariableIdx) ||
-        (selectedIdx === qualitativeVariableIdx && x.length > 0)
+        currentAxis.includes(qualitativeVariableIdx) ||
+        (selectedIdx === qualitativeVariableIdx && currentAxis.length > 0)
       ) {
         alert("질적변인과 양적변인을 동시에 선택할 수 없습니다.");
         return state;
@@ -79,127 +70,22 @@ export const useBarAxisSacleEditorStore = create(set => ({
         ...state,
         axisScale: {
           ...state.axisScale,
-          x: [...x, selectedIdx],
+          [axis]: [...currentAxis, selectedIdx],
         },
       };
-    }),
-  changeSelectedY: (selectedIdx, qualitativeVariableIdx) =>
-    set(state => {
-      const { y } = state.axisScale;
+    });
 
-      if (y.includes(selectedIdx)) {
-        return {
-          ...state,
-          axisScale: {
-            ...state.axisScale,
-            y: y.filter(s => s !== selectedIdx),
-          },
-        };
-      }
+// 상태 관리 훅 생성 함수
+const createTwoArrayAxisScaleEditorStore = (isLineChart = false) =>
+  create(set => ({
+    axisScale: { x: [], y: [], min: 0, max: 0, stepSize: 0 },
+    changeMinValue: changeValue(set, "min"),
+    changeMaxValue: changeValue(set, "max"),
+    changeStepSize: changeValue(set, "stepSize"),
+    changeSelectedX: changeSelected(set, "x", isLineChart),
+    changeSelectedY: changeSelected(set, "y", isLineChart),
+  }));
 
-      if (
-        y.includes(qualitativeVariableIdx) ||
-        (selectedIdx === qualitativeVariableIdx && y.length > 0)
-      ) {
-        alert("질적변인과 양적변인을 동시에 선택할 수 없습니다.");
-        return state;
-      }
-
-      return {
-        ...state,
-        axisScale: {
-          ...state.axisScale,
-          y: [...y, selectedIdx],
-        },
-      };
-    }),
-}));
-
-export const useLineAxisSacleEditorStore = create(set => ({
-  axisScale: { x: [], y: [], min: 0, max: 0, stepSize: 0 },
-  changeMinValue: min =>
-    set(state => {
-      return { ...state, axisScale: { ...state.axisScale, min } };
-    }),
-
-  changeMaxValue: max =>
-    set(state => {
-      return { ...state, axisScale: { ...state.axisScale, max } };
-    }),
-
-  changeStepSize: stepSize =>
-    set(state => {
-      return { ...state, axisScale: { ...state.axisScale, stepSize } };
-    }),
-
-  changeSelectedX: (selectedIdx, qualitativeVariableIdx) =>
-    set(state => {
-      if (selectedIdx !== qualitativeVariableIdx) {
-        alert("만들 수 없는 그래프 유형입니다.");
-        return state;
-      }
-
-      const { x } = state.axisScale;
-
-      if (x.includes(selectedIdx)) {
-        return {
-          ...state,
-          axisScale: {
-            ...state.axisScale,
-            x: x.filter(s => s !== selectedIdx),
-          },
-        };
-      }
-
-      if (
-        x.includes(qualitativeVariableIdx) ||
-        (selectedIdx === qualitativeVariableIdx && x.length > 0)
-      ) {
-        alert("질적변인과 양적변인을 동시에 선택할 수 없습니다.");
-        return state;
-      }
-
-      return {
-        ...state,
-        axisScale: {
-          ...state.axisScale,
-          x: [...x, selectedIdx],
-        },
-      };
-    }),
-  changeSelectedY: (selectedIdx, qualitativeVariableIdx) =>
-    set(state => {
-      if (selectedIdx === qualitativeVariableIdx) {
-        alert("만들 수 없는 그래프 유형입니다.");
-        return;
-      }
-
-      const { y } = state.axisScale;
-
-      if (y.includes(selectedIdx)) {
-        return {
-          ...state,
-          axisScale: {
-            ...state.axisScale,
-            y: y.filter(s => s !== selectedIdx),
-          },
-        };
-      }
-
-      if (
-        y.includes(qualitativeVariableIdx) ||
-        (selectedIdx === qualitativeVariableIdx && y.length > 0)
-      ) {
-        alert("질적변인과 양적변인을 동시에 선택할 수 없습니다.");
-        return state;
-      }
-
-      return {
-        ...state,
-        axisScale: {
-          ...state.axisScale,
-          y: [...y, selectedIdx],
-        },
-      };
-    }),
-}));
+export const useBarAxisSacleEditorStore = createTwoArrayAxisScaleEditorStore();
+export const useLineAxisSacleEditorStore =
+  createTwoArrayAxisScaleEditorStore(true);
