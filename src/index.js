56c76fafe34fedc61a353ef1fc4eb03b6a744027
LeeJaeHoon <dlwogns3413@naver.com>
2023-09-19 13:03:22 +0900

    #115 - feat/Chart datalabels 기본 설정 변경

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 
+import { Chart } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+Chart.register(ChartDataLabels);
+
+Chart.register(ChartDataLabels);
+
+// 전역 설정
+Chart.defaults.plugins.datalabels.display = false;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
