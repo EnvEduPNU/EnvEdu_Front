87193a1651bc714156f1409deae6dde72391de2b
LeeJaeHoon <dlwogns3413@naver.com>
2023-09-26 14:03:29 +0900

    #115 - fix/그래프 높이 너무 커지는 bug fix

--- src/DataLiteracy/GraphInterpreter/GraphInterpreterPage.js
+++ src/DataLiteracy/GraphInterpreter/GraphInterpreterPage.js
@@ -6,8 +6,10 @@ import { useState } from "react";
 import GraphSummary from "./GraphSummary";
 import GraphInterpreter from "./GraphInterpreter";
 import GraphEvaluator from "./GraphEvaluator";
+import { useNavigate } from "react-router-dom";
 
 function GraphInterpreterPage() {
+  const navigate = useNavigate();
   const localStorageData = JSON.parse(localStorage.getItem("graphInterpreter"));
   const [activeStep, setActiveStep] = useState(
     localStorageData?.step ? localStorageData?.step : 1
@@ -15,7 +17,10 @@ function GraphInterpreterPage() {
   const steps = ["그래프 요약", "그래프 해석", "그래프 평가"];
 
   const onClickPrevButton = () => {
-    if (activeStep === 1) return;
+    if (activeStep === 1) {
+      navigate(-1);
+      return;
+    }
     setActiveStep(state => state - 1);
   };
 
