bb1677589ff7be869d10533ca8b157087d15a673
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-16 02:41:31 +0900

    #153 - fix/ 데이터 스케일링시 로그10, 소수점 첫째자리에서 반올림

--- src/DataLiteracy/utils/scaling.js
+++ src/DataLiteracy/utils/scaling.js
@@ -1,3 +1,11 @@
+function log10(val) {
+  return Math.log(val) / Math.LN10;
+}
+
+function roundToOneDecimalPlace(num) {
+  return Math.round(num * 10) / 10;
+}
+
 function isNumericColumn(data, colIndex) {
   for (let i = 1; i < data.length; i++) {
     if (data[i][colIndex] !== null && typeof data[i][colIndex] !== "number") {
@@ -15,8 +23,9 @@ export function minMaxScalingForDataset(dataset) {
     const min = Math.min(...columnData);
     const max = Math.max(...columnData);
     for (let rowIndex = 1; rowIndex < scaledDataset.length; rowIndex++) {
-      scaledDataset[rowIndex][colIndex] =
-        (scaledDataset[rowIndex][colIndex] - min) / (max - min);
+      scaledDataset[rowIndex][colIndex] = roundToOneDecimalPlace(
+        (scaledDataset[rowIndex][colIndex] - min) / (max - min)
+      );
     }
   }
   return scaledDataset;
@@ -34,8 +43,9 @@ export function zScoreNormalizationForDataset(dataset) {
       columnData.length;
     const stdDev = Math.sqrt(variance);
     for (let rowIndex = 1; rowIndex < normalizedDataset.length; rowIndex++) {
-      normalizedDataset[rowIndex][colIndex] =
-        (normalizedDataset[rowIndex][colIndex] - mean) / stdDev;
+      normalizedDataset[rowIndex][colIndex] = roundToOneDecimalPlace(
+        (normalizedDataset[rowIndex][colIndex] - mean) / stdDev
+      );
     }
   }
   return normalizedDataset;
@@ -47,7 +57,8 @@ export function logTransformForDataset(dataset) {
     if (!isNumericColumn(logDataset, colIndex)) continue;
     for (let rowIndex = 1; rowIndex < logDataset.length; rowIndex++) {
       const value = logDataset[rowIndex][colIndex];
-      logDataset[rowIndex][colIndex] = value > 0 ? Math.log(value) : value;
+      logDataset[rowIndex][colIndex] =
+        value > 0 ? roundToOneDecimalPlace(log10(value)) : value;
     }
   }
   return logDataset;
@@ -59,7 +70,8 @@ export function sqrtTransformForDataset(dataset) {
     if (!isNumericColumn(sqrtDataset, colIndex)) continue;
     for (let rowIndex = 1; rowIndex < sqrtDataset.length; rowIndex++) {
       const value = sqrtDataset[rowIndex][colIndex];
-      sqrtDataset[rowIndex][colIndex] = value >= 0 ? Math.sqrt(value) : value;
+      sqrtDataset[rowIndex][colIndex] =
+        value >= 0 ? roundToOneDecimalPlace(Math.sqrt(value)) : value;
     }
   }
   return sqrtDataset;
