a22eedcef39f2fd241ddca80d623d5ccb26bfc4c
LeeJaeHoon <dlwogns3413@naver.com>
2023-10-16 15:15:21 +0900

    #153 - feat/ drawGraph와 데이터 연결

--- src/DataLiteracy/sampleData/sampleData.js
+++ src/DataLiteracy/sampleData/sampleData.js
@@ -1,4 +1,4 @@
-export const data = [
+export const data = JSON.parse(localStorage.getItem("data")) || [
   ["농업지대", "평균기온", "강수량", "일조시간"],
   ["태백고냉", 21.9, 181.9, 149.7],
   ["소백간산", 25.3, 675.6, 140],
