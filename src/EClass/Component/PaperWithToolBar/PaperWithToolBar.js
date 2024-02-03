a7d9b90abc58374be6dbd867f2215f1404d5bb2c
LeeJaeHoon <dlwogns3413@naver.com>
2024-02-04 03:08:35 +0900

    feat: E-Class 학생 assignment 선택 css  완성

--- src/EClass/Component/PaperWithToolBar/PaperWithToolBar.js
+++ src/EClass/Component/PaperWithToolBar/PaperWithToolBar.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import FormCheckInput from "react-bootstrap/esm/FormCheckInput";
 import Toolbar from "../ToolBar/Toolbar";
 import * as Styled from "./Styled";
+import { FormLabel } from "react-bootstrap";
 
 const PaperWithToolBar = ({ pageNum, activities }) => {
   return (
     <Styled.Wrapper>
       <Toolbar pageNum={pageNum} />
       <Styled.Paper>
-        {activities.map((a, idx) => (
-          <div key={idx}>{a}</div>
+        {activities.map((activity, idx) => (
+          <Styled.ActivityWrapper key={idx}>
+            <Styled.ActivityEditHeader>
+              <FormLabel style={{ cursor: "pointer" }}>
+                {/* <span>학생 보고서</span> */}
+                <FormCheckInput defaultChecked style={{ marginLeft: "7px" }} />
+              </FormLabel>
+            </Styled.ActivityEditHeader>
+            <div>{activity}</div>
+          </Styled.ActivityWrapper>
         ))}
       </Styled.Paper>
     </Styled.Wrapper>
--- src/EClass/Component/PaperWithToolBar/Styled.js
+++ src/EClass/Component/PaperWithToolBar/Styled.js
@@ -7,10 +7,27 @@ export const Paper = styled.div`
   width: 595px;
   background-color: white;
   /* text-align: center; */
-  padding: 20px;
+  padding: 10px;
   display: flex;
   flex-direction: column;
   gap: 10px;
   cursor: pointer;
   overflow-y: hidden;
+`;
+
+export const ActivityWrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #eee;
+  display: flex;
+  flex-direction: column;
+  /* gap: 3px; */
+  border-radius: 5px;
+`;
+
+export const ActivityEditHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 5px;
 `;
