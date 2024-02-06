6997b66e2db614cf7fd002573fbe1607b78368b0
Jisu Lee <dlwltn0430@naver.com>
2024-02-06 11:18:50 +0900

    #218-style/아이디 및 비번 찾기 화면 리뉴얼

--- src/User/Find/Find.js
+++ src/User/Find/Find.js
@@ -1,42 +1,55 @@
 import React, { Component } from 'react';
-import './Find.css'
+import './Find.scss'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 class Find extends Component {
     render() {
         return (
-            <>
-                <form>
-                    <fieldset>
-                    <legend>아이디 찾기</legend><br></br>
-                    <div className="id-find">
-                    <input type="text" placeholder='이름'/>
-                    </div>
-                    <div className="id-find">
-                    <input type="email" placeholder='이메일' />
-                    </div>
-                    <button type="button" class="btn btn-secondary">인증번호 받기</button>
-                    </fieldset>
-                </form>
-                <form>
-                    <fieldset>
-                    <legend>비밀번호 찾기</legend><br></br>
-                    <div className="password-find">
-                    <input type="text" placeholder='이름'/>
-                    </div><div className="password-find">
-                    <input type="text" placeholder='아이디'/>
-                    </div><div className="password-find">
-                    <input type="email" placeholder='이메일'/>
-                    </div>
-                    <button type="button" class="btn btn-secondary">인증번호 받기</button>
-                    </fieldset>
-                </form>
-            </>
-                
+            <div style={{ flexDirection: 'column', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Tabs
+                    defaultActiveKey="find-id"
+                >
+                    <Tab eventKey="find-id" title={<span style={{ fontSize: '1.5em', fontWeight: 'bold', color: '#000' }}>아이디 찾기</span>}>
+                        <div className='find-id-pw'>
+                            <form style={{ marginTop: '3rem' }}>
+                                <fieldset>
+                                    {/*<h4 style={{ fontWeight: 'bold', textAlign: 'center' }}>아이디 찾기</h4>*/}
+                                    <div className="id-find">
+                                        <input type="text" placeholder='이름' className="loginInput" />
+                                    </div>
+                                    <div className="id-find">
+                                        <input type="email" placeholder='이메일' className="loginInput" />
+                                    </div>
 
+                                    <button type="button" class="btn login-btn">인증번호 받기</button>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </Tab>
+                    
+                    <Tab eventKey="find-pw" title={<span style={{ fontSize: '1.5em', fontWeight: 'bold', color: '#000' }}>비밀번호 찾기</span>}>
+                        <div className='find-id-pw'>
+                            <form style={{ marginTop: '3rem' }}>
+                                <fieldset>
+                                    {/*<h4 style={{ fontWeight: 'bold', textAlign: 'center' }}>비밀번호 찾기</h4>*/}
+                                    <div className="password-find">
+                                        <input type="text" placeholder='아이디' className="loginInput"/>
+                                    </div>
+                                    <div className="password-find">
+                                        <input type="text" placeholder='이름' className="loginInput"/>
+                                    </div>
+                                    <div className="password-find">
+                                        <input type="email" placeholder='이메일' className="loginInput"/>
+                                    </div>
 
-
-
-            
+                                    <button type="button" class="btn login-btn">인증번호 받기</button>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </Tab>
+                </Tabs>
+            </div>
         );
     }
 }
