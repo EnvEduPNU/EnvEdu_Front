1c3f0317c5396148bd86fff4a19798202a227780
PUO <86181292+kl204@users.noreply.github.com>
2024-06-09 13:29:13 +0900

    [update] E-class url 변경 #251

--- src/EClass/Page/EClassPage/LiveStudentPage.js
+++ src/EClass/Page/EClassPage/LiveStudentPage.js
@@ -7,7 +7,7 @@ function LiveStudentPage() {
   const [page, setPage] = useState("defaultPage");
 
   useEffect(() => {
-    const sock = new SockJS("http://localhost:9090/ws");
+    const sock = new SockJS("http://localhost:8080/ws");
     const stompClient = Stomp.over(sock);
 
     stompClient.connect({}, function (frame) {
