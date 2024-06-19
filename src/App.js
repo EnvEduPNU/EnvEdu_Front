c7cbd1cd9b93ed89206a81f72dc784bc3c0745f1
PUO <86181292+kl204@users.noreply.github.com>
2024-06-19 17:55:59 +0900

    아우 진짜

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Switch } from "react-router-dom";
 import RegisterForm from "./User/Register/RegisterForm";
 import Home from "./Homepage/Home";
 import AddMACForm from "./Admin/Device/AddMACForm";
