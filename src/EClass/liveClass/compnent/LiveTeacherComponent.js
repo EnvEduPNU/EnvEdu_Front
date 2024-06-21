0034615b894acd8f068caac5906d7177ee496dda
PUO <86181292+kl204@users.noreply.github.com>
2024-06-21 17:46:30 +0900

    테스트

--- src/EClass/liveClass/compnent/LiveTeacherComponent.js
+++ src/EClass/liveClass/compnent/LiveTeacherComponent.js
@@ -5,67 +5,49 @@ import { customAxios } from "../../../Common/CustomAxios";
 
 const LiveTeacherComponent = () => {
   const localVideoRef = useRef(null);
-  const [stompClient, setStompClient] = useState(null);
-  const [peerConnections, setPeerConnections] = useState({}); // 여러 PeerConnection 객체 관리
+  const [stompClients, setStompClients] = useState([]);
+  const [peerConnections, setPeerConnections] = useState([]); // 여러 PeerConnection 객체 관리
   const [sessionIds, setSessionIds] = useState([]); // 세션 ID 배열 상태
-  const [activeSessionId, setActiveSessionId] = useState(""); // 활성화된 세션 ID
 
   useEffect(() => {
-    const token = localStorage.getItem("access_token").replace("Bearer ", "");
-    const socket = new SockJS(
-      `${process.env.REACT_APP_API_URL}/screen-share?token=${token}`
-    );
-    const client = Stomp.over(socket);
-    setStompClient(client);
-
+    // 1. 세션 아이디 들을 가져와서 각 세션 마다 소켓 생성
     fetchSessionIds().then((ids) => {
       setSessionIds(ids);
-      if (ids.length > 0) {
-        setActiveSessionId(ids[0]); // 첫 번째 세션을 활성 세션으로 설정
-      }
     });
 
     return () => {
-      if (client) {
-        client.disconnect();
+      if (stompClients.length > 0) {
+        stompClients.forEach((clientObj) => {
+          const [sessionId, client] = Object.entries(clientObj)[0];
+          client.disconnect();
+        });
       }
     };
   }, []);
 
-  const fetchSessionIds = async () => {
-    try {
-      const response = await customAxios.get("/api/sessions/get-session-ids");
-      console.log("받아온 세션들 : " + response.data);
-      return response.data; // 세션 ID 배열 반환
-    } catch (error) {
-      console.error("Failed to fetch session IDs:", error);
-      alert("Failed to fetch session IDs");
-      return [];
-    }
-  };
-
+  // 2. 각 세션의 소켓 마다 peerConnection 생성
   useEffect(() => {
-    if (stompClient && sessionIds.length > 0) {
-      sessionIds.forEach((sessionId) => {
-        const pc = new RTCPeerConnection();
-        peerConnections[sessionId] = pc;
-        setPeerConnections(peerConnections);
+    if (stompClients.length > 0 && sessionIds.length > 0) {
+      const newPeerConnections = [];
 
-        stompClient.connect({}, () => {
-          console.log("Connected to STOMP");
-          stompClient.subscribe(`/topic/answer/${sessionId}`, (message) => {
+      stompClients.forEach((clientObj) => {
+        const [sessionId, client] = Object.entries(clientObj)[0];
+        const pc = new RTCPeerConnection();
+        newPeerConnections.push({ sessionId, pc });
+
+        client.connect({}, () => {
+          console.log(`Connected to STOMP for session: ${sessionId}`);
+          client.subscribe(`/topic/answer/${sessionId}`, (message) => {
             handleAnswer(JSON.parse(message.body), sessionId);
           });
-          stompClient.subscribe(`/topic/candidate/${sessionId}`, (message) => {
+          client.subscribe(`/topic/candidate/${sessionId}`, (message) => {
             handleCandidate(JSON.parse(message.body), sessionId);
           });
         });
 
         pc.ontrack = (event) => {
-          if (sessionId === activeSessionId) {
-            console.log("Received remote stream from active session");
-            localVideoRef.current.srcObject = event.streams[0];
-          }
+          console.log("Received remote stream from active session");
+          localVideoRef.current.srcObject = event.streams[0];
         };
 
         pc.onicecandidate = (event) => {
@@ -76,17 +58,51 @@ const LiveTeacherComponent = () => {
           }
         };
       });
-    }
-  }, [stompClient, sessionIds]);
 
+      setPeerConnections(newPeerConnections);
+    }
+  }, [stompClients, sessionIds]);
+
+  // db의 세션 아이디를 받아서 각 세션 마다 소켓 생성 메서드
+  const fetchSessionIds = async () => {
+    try {
+      const response = await customAxios.get("/api/sessions/get-session-ids");
+      const sessionIds = response.data;
+
+      console.log("받아온 세션들 : " + sessionIds);
+
+      let clientTemp = [];
+
+      sessionIds.forEach((sessionId) => {
+        const token = localStorage
+          .getItem("access_token")
+          .replace("Bearer ", "");
+        const socket = new SockJS(
+          `${process.env.REACT_APP_API_URL}/screen-share?token=${token}`
+        );
+        const client = {};
+        client[sessionId] = Stomp.over(socket);
+        clientTemp.push(client);
+      });
+
+      setStompClients(clientTemp);
+
+      return sessionIds;
+    } catch (error) {
+      console.error("Failed to fetch session IDs:", error);
+      alert("Failed to fetch session IDs");
+      return [];
+    }
+  };
+
+  // 화면 공유 시작
   const startScreenShare = async () => {
     try {
       const stream = await navigator.mediaDevices.getDisplayMedia({
         video: { width: 1280, height: 720, frameRate: 15 },
       });
 
-      Object.keys(peerConnections).forEach((sessionId) => {
-        const pc = peerConnections[sessionId];
+      peerConnections.forEach(({ sessionId, pc }) => {
         stream.getTracks().forEach((track) => pc.addTrack(track, stream));
 
         pc.createOffer().then((offer) => {
@@ -95,9 +111,7 @@ const LiveTeacherComponent = () => {
         });
       });
 
-      if (activeSessionId) {
-        localVideoRef.current.srcObject = stream;
-      }
+      localVideoRef.current.srcObject = stream;
     } catch (error) {
       console.error("Error sharing screen:", error);
       alert("Screen sharing is not supported on this device.");
@@ -106,20 +120,29 @@ const LiveTeacherComponent = () => {
 
   const sendSignal = (destination, message) => {
     console.log(`Sending signal to ${destination}:`, message);
-    stompClient.send(destination, {}, JSON.stringify(message));
+    stompClients.forEach((clientObj) => {
+      const client = Object.values(clientObj)[0];
+      client.send(destination, {}, JSON.stringify(message));
+    });
   };
 
   const handleAnswer = async (message, sessionId) => {
-    const pc = peerConnections[sessionId];
-    if (pc) {
+    const connection = peerConnections.find(
+      (conn) => conn.sessionId === sessionId
+    );
+    if (connection) {
+      const { pc } = connection;
       console.log(`Processing answer for session ${sessionId}`);
       await pc.setRemoteDescription(new RTCSessionDescription(message.answer));
     }
   };
 
   const handleCandidate = async (message, sessionId) => {
-    const pc = peerConnections[sessionId];
-    if (pc) {
+    const connection = peerConnections.find(
+      (conn) => conn.sessionId === sessionId
+    );
+    if (connection) {
+      const { pc } = connection;
       console.log(`Processing candidate for session ${sessionId}`);
       await pc.addIceCandidate(new RTCIceCandidate(message.candidate));
     }
