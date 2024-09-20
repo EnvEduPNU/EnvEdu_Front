2476c3602d4fe9d069192563fd8da3ed280c263d
PUO <kl204@naver.com>
2024-09-20 18:01:10 +0900

    [update] 소켓 메시지 확인

--- src/EClass/liveClass/page/LiveTeacherPage.js
+++ src/EClass/liveClass/page/LiveTeacherPage.js
@@ -101,7 +101,7 @@ export const LiveTeacherPage = () => {
   }, []);
 
   const sendTrueMessage = () => {
-    setSharedScreenState(true);
+    // setSharedScreenState(true);
 
     const message = {
       screenShared: true,
@@ -119,7 +119,7 @@ export const LiveTeacherPage = () => {
   };
 
   const sendFalseMessage = () => {
-    setSharedScreenState(false);
+    // setSharedScreenState(false);
 
     const message = {
       screenShared: false,
