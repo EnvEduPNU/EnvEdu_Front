e9091065a4d2f315686c2f4d187a2899ed4f0c69
PUO <kl204@naver.com>
2024-09-20 19:38:07 +0900

    test03

--- src/EClass/liveClass/page/LiveTeacherPage.js
+++ src/EClass/liveClass/page/LiveTeacherPage.js
@@ -74,32 +74,6 @@ export const LiveTeacherPage = () => {
     };
   }, []);
   useEffect(() => {
-    // 소켓 연결 및 메시지 보내는 함수
-    const sendMessage = async (state) => {
-      const message = {
-        screenShared: state,
-      };
-
-      if (
-        ScreanSharestompClients.current &&
-        ScreanSharestompClients.current.connected
-      ) {
-        try {
-          console.log('소켓 보내기');
-          await ScreanSharestompClients.current.publish({
-            destination: '/app/screen-share-flag', // 메시지를 보낼 경로
-            body: JSON.stringify(message), // 메시지 본문
-            headers: {}, // 선택적 헤더
-          });
-        } catch (error) {
-          console.error('메시지 전송 오류:', error);
-        }
-      } else {
-        console.error('STOMP 클라이언트가 연결되지 않았습니다.');
-        // 재연결 로직을 추가할 수 있음
-      }
-    };
-
     // 소켓이 한 번만 연결되도록
     if (!ScreanSharestompClients.current) {
       const token = localStorage.getItem('access_token').replace('Bearer ', '');
@@ -112,7 +86,7 @@ export const LiveTeacherPage = () => {
 
       ScreanSharestompClients.current.onConnect = (frame) => {
         console.log('화면 공유 소켓 연결 성공 : ', frame);
-        sendMessage(sharedScreenState); // 연결된 후 현재 상태로 메시지 보내기
+        sendMessage(true);
       };
 
       ScreanSharestompClients.current.activate(); // 소켓 활성화
@@ -133,7 +107,33 @@ export const LiveTeacherPage = () => {
         });
       }
     };
-  }, [sharedScreenState]); // sharedScreenState가 변경될 때 실행
+  }, []); // sharedScreenState가 변경될 때 실행
+
+  // 소켓 연결 및 메시지 보내는 함수
+  const sendMessage = async (state) => {
+    const message = {
+      screenShared: state,
+    };
+
+    if (
+      ScreanSharestompClients.current &&
+      ScreanSharestompClients.current.connected
+    ) {
+      try {
+        console.log('소켓 보내기');
+        await ScreanSharestompClients.current.publish({
+          destination: '/app/screen-share-flag', // 메시지를 보낼 경로
+          body: JSON.stringify(message), // 메시지 본문
+          headers: {}, // 선택적 헤더
+        });
+      } catch (error) {
+        console.error('메시지 전송 오류:', error);
+      }
+    } else {
+      console.error('STOMP 클라이언트가 연결되지 않았습니다.');
+      // 재연결 로직을 추가할 수 있음
+    }
+  };
 
   const closeEclass = async () => {
     await customAxios
